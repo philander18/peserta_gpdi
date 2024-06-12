bd0cfe4dc9f20367e0fdcb030dcb23e5188907dc
philander18 <philander.totalis@gmail.com>
2024-06-12 16:35:53 +0700

    Perbaiki view kode

--- app/Views/Portal/index.php
+++ app/Views/Portal/index.php
@@ -3,7 +3,7 @@
 <form autocomplete="off" action="<?= base_url(); ?>" method="POST">
     <div class="portal mt-4">
         <h3>Masukkan Kode</h3>
-        <div class="input-group mb-3">
+        <div class="input-group p-2">
             <input type="text" class="form-control" placeholder="Kode" aria-label="Kode" aria-describedby="button-addon2" name="kode">
             <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Button</button>
         </div>
